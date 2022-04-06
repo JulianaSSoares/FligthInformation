33d45485537d80313cc30b65a1fd7bcf04679491
Juliana Soares <juliana_ssoares@live.com>
2022-04-05 21:40:09 -0300

    projeto flight information

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
 
-        String path = "C:\\Users\\Juh\\IdeaProjects\\FlightInformations\\flights.csv";
+        String path = "C:\\Users\\Juh\\IdeaProjects\\FlightInformation\\flights.csv";
         List<Voo> flights = new ArrayList<Voo>();
 
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(path))) {
@@ -15,26 +18,27 @@ public class Main {
             while (line != null) {
                 String[] array = line.split(";");
 
+                String origin = array[0];
+                String destination = array[1];
+                String airline = array[2];
+                String departure = array[3];
+                String arrival = array[4];
+                Double price = Double.valueOf(array[5]);
 
-                    String origin = array[0];
-                    String destination = array[1];
-                    String airline = array[2];
-                    String departure = array[3];
-                    String arrival = array[4];
-                    Double price = Double.valueOf(array[5]);
+                Voo voo = new Voo(origin, destination, airline, departure, arrival, price);
 
-                    Voo voo = new Voo(origin, destination, airline, departure, arrival, price);
-
-                    flights.add(voo);
-                    line = bufferedReader.readLine();
-
+                flights.add(voo);
+                line = bufferedReader.readLine();
             }
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
 
-        for (Voo v: flights) {
+        for (Voo v : flights) {
             System.out.println(v);
         }
+
+            //flights.stream().filter(voo -> voo.getOrigin().)
     }
 }
+
--- src/Voo.java
+++ src/Voo.java
@@ -1,45 +1,76 @@
 import java.time.OffsetTime;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 public class Voo {
     private String origin;
     private String destination;
     private String airline;
-    private String departure;
-    private String arrival;
+    private ZonedDateTime departure;
+    private ZonedDateTime arrival;
     private double price;
+    private long duration;
 
     public Voo(String origin, String destination, String airline, String departure, String arrival, double price) {
         this.origin = origin;
         this.destination = destination;
         this.airline = airline;
+        this.departure = ZonedDateTime.parse(departure, DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss '('zzz')'"));
+        this.arrival = ZonedDateTime.parse(arrival, DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss '('zzz')'"));
+        this.price = price;
+
+        duration = this.departure.until(this.arrival, ChronoUnit.HOURS);
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public String getAirline() {
+        return airline;
+    }
+
+    public void setAirline(String airline) {
+        this.airline = airline;
+    }
+
+    public ZonedDateTime getDeparture() {
+        return departure;
+    }
+
+    public void setDeparture(ZonedDateTime departure) {
         this.departure = departure;
+    }
+
+    public ZonedDateTime getArrival() {
+        return arrival;
+    }
+
+    public void setArrival(ZonedDateTime arrival) {
         this.arrival = arrival;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
         this.price = price;
     }
 
-    public String getOrigin() { return origin; }
-
-    public void setOrigin(String origin) { this.origin = origin; }
-
-    public String getDestination() { return destination; }
-
-    public void setDestination(String destination) { this.destination = destination; }
-
-    public String getAirline() { return airline; }
-
-    public void setAirline(String airline) { this.airline = airline; }
-
-    public String getDeparture() { return departure; }
-
-    public void setDeparture(String departure) { this.departure = departure; }
-
-    public String getArrival() { return arrival; }
-
-    public void setArrival(String arrival) { this.arrival = arrival; }
-
-    public double getPrice() { return price; }
-
-    public void setPrice(double price) { this.price = price; }
 
     @Override
     public String toString() {
@@ -50,6 +81,7 @@ public class Voo {
                 ", departure='" + departure + '\'' +
                 ", arrival='" + arrival + '\'' +
                 ", price=" + price +
+                ", duration=" + duration +
                 '}';
     }
 }
